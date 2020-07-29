906e2d9877eae74d9a18a98656a2b82fcd0c082c
Josh Justice <josh.justice@accesscfa.com>
2020-07-29 17:16:12 -0400

    Add sample code

---
+++ index.js
@@ -0,0 +1,26 @@
+const getWidgets = () => Promise.resolve(['Widget 1', 'Widget 2']);
+const getWeebles = () => Promise.resolve(['Weeble A', 'Weeble B']);
+
+// getWidgets().then(widgets => {
+//   console.log(widgets);
+// });
+//
+// getWeebles().then(console.log);
+
+// getWidgets()
+//   .then(() => {
+//     return getWeebles();
+//   })
+//   .then(result => {
+//     console.log(result);
+//   });
+
+getWidgets()
+  .then(widgets => {
+    return getWeebles().then(weebles => {
+      return { widgets, weebles };
+    });
+  })
+  .then(result => {
+    console.log(result);
+  });
